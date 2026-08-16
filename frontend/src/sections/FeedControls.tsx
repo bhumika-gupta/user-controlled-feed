e056ec6a21548771ed38a1ae0fd0005717ebedf4
guptab8 <guptab8@mcmaster.ca>
2026-08-16 15:27:17 -0400

    add basic rendering of handling user's feed preferences

--- frontend/src/sections/FeedControls.tsx
+++ frontend/src/sections/FeedControls.tsx
@@ -1,3 +1,20 @@
 // handles the user's feed preferences
 // initially: latest, following
 // eventually: user directed. topics, ranking preferences
+
+function FeedControls() {
+    return (
+        <div>
+            <p>Feed: </p>
+            <button>
+                [Latest]
+            </button>
+            <button>
+                [Following]
+            </button>
+        </div>
+    )
+
+}
+
+export default FeedControls
